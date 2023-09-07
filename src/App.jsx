94cc7813ad41b692857df72d83483376081d706d
Eliseu Vicente <eliseu965@gmail.com>
2023-09-07 09:38:28 -0300

    create project, first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { GlobalStyle } from "./styles/themes/global"
+
+
+export function App() {
+  
+  return (
+    <>
+    <GlobalStyle>
+      <div>
+        Ola
+     </div>
+     </GlobalStyle>
+    </>
+  )
+}
